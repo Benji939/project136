70938cc45be998e90a1fbdc65681f231bbb2520a
Benji939 <76928577+Benji939@users.noreply.github.com>
2021-11-20 22:11:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 status="";
+video="";
 function preload(){
-canvas=createCapture(480,380);
-canvas.center();
-video=createVideo(380,480);
-video.hide();
-}
-function setup(){
 
 }
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+video.size(480,380);
+video.hide();
+}
 function draw(){
-image(video,0,0,380,480);
+image(video,0,0,480,380);
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
